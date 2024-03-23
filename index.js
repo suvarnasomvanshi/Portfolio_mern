cfd6c47bd74485f603070f6ed2a6121e7ca2d8c7
suvarna <somavanshisuvarna48@gmail.com>
2024-03-23 19:48:19 +0530

    connecting to mongodb database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from express;
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000
 
-const PORT = 8000;
 
+mongoose.connect()
 app.listen(PORT,()=>{
     console.log(`listening to port ${PORT}`)
 })
